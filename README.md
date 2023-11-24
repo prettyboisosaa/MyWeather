6d59e418229b6fc5b23e2c86a4a263f07e02ec05
prettyboisosaa <midumfernando123@gmail.com>
2023-11-24 10:21:04 +0100

    add Readme

---
+++ README.md
@@ -0,0 +1,1 @@
+this is a Weather app that uses api from WeatherApi.com to retrive datas in a json format and order them in a table created with getbootstrap and other tecnologies
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+from datetime import datetime
 
 #https://www.weatherapi.com/docs/
 
@@ -7,23 +8,47 @@ app = Flask(__name__)
 
 api_key = 'f6c164a0f6744f94982171505232311'
 
+
 @app.route('/')
 def main():
     return render_template("index.jinja")
 
-@app.route('/submit', methods=['POST'])
+@app.route('/submit', methods=['GET', 'POST'])
 def submit():
-    city = request.form['message']
-    url = f"http://api.weatherapi.com/v1/current.json"
-    params = {
-        'key' : api_key,
-        'q' : city
-    }
+    data = None
+    if request.method == 'POST':
+        city = request.form['message']
+        url = f"http://api.weatherapi.com/v1/current.json"
+        params = {
+            'key' : api_key,
+            'q' : city
+        }
 
-    response = requests.get(url, params=params)
-    data = response.json()
+        response = requests.get(url, params=params)
+        data = response.json()
 
-    return render_template("index.jinja", data=data)
+    return render_template("meteo.jinja", data=data)
+
+@app.route('/astro', methods=['GET', 'POST'])
+def astro():
+    data = None
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d")
+    if request.method == 'POST':
+        city = request.form['message']
+        url = f"http://api.weatherapi.com/v1/astronomy.json"
+        params = {
+            'key' : api_key,
+            'q' : city,
+            'dt' : formatted_date
+        }
+
+        response = requests.get(url, params=params)
+        data = response.json()
+
+    return render_template("astro.jinja", data=data)
+
+
 
 if __name__ == '__main__':
     app.run()
